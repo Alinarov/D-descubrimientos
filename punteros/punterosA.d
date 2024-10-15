c1990e7c62013223dddb218188d0ff56322a927c
Alinarov <150054498+Alinarov@users.noreply.github.com>
2024-10-15 16:11:46 -0500

    Update punterosA.d

--- punteros/punterosA.d
+++ punteros/punterosA.d
@@ -5,11 +5,15 @@ import std; // Importa el módulo estándar de D
 alias print = writeln;
 
 void main() {
+
     string s = "sa"; // Declara una variable de tipo string y la inicializa con el valor "sa"
+    string q = "e";  // Declara otra variable de tipo string con el valor "e"
     
-    // Declara un puntero de tipo string y lo inicializa con la dirección de 's'
-    string* j = &s; 
+    // Declara un arreglo de punteros de tipo string y lo inicializa con las direcciones de 's' y 'q'
+    string*[] j = [&s, &q]; 
     
-    // Imprime el valor al que apunta el puntero 'j'
-    print(*j); // Aquí se desreferencia el puntero 'j' para obtener el valor de 's'
+    // Recorre el arreglo de punteros 'j' y desreferencia cada puntero para obtener el valor
+    foreach (ptr; j) {
+        print(*ptr); // Desreferencia el puntero y luego imprime el valor al que apunta
+    }
 }
